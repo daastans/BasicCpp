5da6bd464632de2c674f48cf506a34030908ab25
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-13 05:57:40 +1100

    Conversion routine is in destination:Cast operator is defined

---
+++ Data_conversion_1.cpp
@@ -0,0 +1,58 @@
+
+/*In this case conversion routine is present in destination object*/
+#include<iostream>
+#include<string>
+
+class dates
+{
+private:
+    std::string dstr;
+public:
+    dates()
+    {
+        dstr="";
+    }
+    dates(std::string s)
+    {
+        dstr=s;
+    }
+    void displaydata()
+    {
+        std::cout<<dstr<<std::endl;
+    }
+};
+class datei
+{
+private:
+    int dd,mm,yy;
+public:
+    datei()
+    {
+        dd=mm=yy=0;
+    }
+    datei(int d,int m,int y)
+    {
+        dd=d;
+        mm=m;
+        yy=y;
+    }
+    operator dates()//Conversion/casting operator
+    {
+        std::string dates;
+        dates=std::to_string(dd)+"\\"+std::to_string(mm)+"\\"+std::to_string(yy);
+        return dates;
+    }
+    void displaydata()
+    {
+        std::cout<<dd<<"\\"<<mm<<"\\"<<yy<<std::endl;
+    }
+};
+int main()
+{
+    dates ds;
+    datei di(11,8,1997);
+    di.displaydata();
+    ds=di;//First it tries to find overload assignment operator otherwise Conversion operator
+    ds.displaydata();
+    return 0;
+}
